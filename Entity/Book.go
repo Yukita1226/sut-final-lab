f6c0c2b7ffee293466d858ae75d6983d835dd6e5
Yukita1226 <thammarat.songwiwat@gmail.com>
2026-01-07 11:56:58 +0700

    f

--- Entity/Book.go
+++ Entity/Book.go
@@ -8,5 +8,5 @@ type Book struct {
 	gorm.Model
 	Title string  `valid:"stringlength(3|100)~Title must atleast 3 and limit at 100"`
 	Price float64 `valid:"range(50|5000)~Price must be between 50 and 5000"`
-	Code  string  `valid:"matches(^BK[0-9]{6})~Code must start with BK followed by 6 digits (0-9)"`
+	Code  string  `valid:"matches(^BK[0-9]{6}$)~Code must start with BK followed by 6 digits (0-9)"`
 }
